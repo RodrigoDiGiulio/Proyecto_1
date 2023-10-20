32b89eea0fc45fbc33bee6e4a572f4f692a1fdde
RodrigoDiGiulio <145570964+RodrigoDiGiulio@users.noreply.github.com>
2023-10-20 11:14:19 -0400

    90% Backwork done
    Ya esta lista toda la parte de kosaraju y todo lo previo que se necesita para ello, falta la validacion del archivo que se va a guardar. Esto no implica que no se pueda guardar, editar y abrir archivos, solo que si esta en el formato incorrecto no va a funcionar

---
+++ Projecto_1/src/projecto_1/main.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projecto_1;
+
+import JFileChooserClasses.Frame;
+import projecto_1.Classes.Stack;
+import projecto_1.Classes.AdjMatrix;
+import projecto_1.Classes.List;
+import projecto_1.Classes.DataInterpreter;
+
+/**
+ * Este es el main donde se ejecuta todo el programa
+ * @author RDG
+ * @version 10/20/2023
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Frame miVentana = new Frame();
+        miVentana.setVisible(true);
+        
+    }
+}
